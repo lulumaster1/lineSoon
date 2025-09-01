5aa261904b079a79f2c826c494fbd557596c8fae
beulje@konkuk.ac.kr <beulje@konkuk.ac.kr>
2025-09-01 20:43:49 +0900

    Initial commit

---
+++ src/main/java/lulu/lineSoon/LineSoonApplication.java
@@ -0,0 +1,13 @@
+package lulu.lineSoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineSoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineSoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lulu/lineSoon/LineSoonApplicationTests.java
@@ -0,0 +1,13 @@
+package lulu.lineSoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineSoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
